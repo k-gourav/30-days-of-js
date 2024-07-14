7ec96dbaee00257b454a2560336f8bd0d2c3e94c
k-gourav <code2change.gourav@gmail.com>
2024-07-15 01:24:26 +0530

    Day2 updated

--- Day_02/index.js
+++ Day_02/index.js
@@ -21,3 +21,74 @@ const number1 = 334;
 const number2 = 23;
 const quotient = number1 / number2;
 console.log(quotient);
+
+// Task 5 :
+const figure1 = 155;
+const figure2 = 23;
+const modulo = figure1 % figure2;
+console.log(modulo);
+
+// Task 6 :
+let firstNum = 88;
+const operator = 3;
+const result = firstNum += operator;
+console.log(result);
+
+// Task 7 :
+let secondNum = 66;
+const operator2 = 15;
+const result2 = singleNum -= operator2;
+console.log(result2);
+
+// Task 8 :
+const bigVal = 33 > 67;
+const smallVal = 57 < 18;
+console.log(bigVal);
+console.log(smallVal);
+
+// Task 9 :
+const bigEqualVal = 98 >= 67;
+const smallEqualVal = 57 <= 57;
+console.log(bigEqualVal);
+console.log(smallEqualVal);
+
+// Task 10 :
+console.log(undefined == 'undefined');
+console.log(15 === '15');
+
+// Task 11 :
+console.log(true && false);
+console.log(1 && true);
+
+// Task 12 :
+console.log(true || false);
+console.log(false || false);
+
+// Task 13 :
+console.log(!false);
+console.log(!15);
+
+// Task 14 :
+const integer = -45;
+const integerType = integer >=0 ? "positive" : "negative";
+console.log(integerType);
+
+// Feature Request 
+console.log(num1 + num2);
+console.log(num1 - num2);
+console.log(num1 * num2);
+console.log(num1 / num2);
+console.log(num2 % num1);
+
+console.log(num1 > num2);
+console.log(num1 < num2);
+console.log(num1 >= num2);
+console.log(num1 <= num2);
+console.log(num1 == '13');
+console.log(num1 === '13');
+
+console.log(num1 > num2 && num1 < num2);
+console.log(num1 > num2 || num1 < num2);
+console.log(!(num1 > num2));
+
+console.log(num1 >= 0 ? "positive" : "negative");
