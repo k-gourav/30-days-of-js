8368f01b7beca9e835f76adb896d5efddd2ea568
k-gourav <code2change.gourav@gmail.com>
2024-07-17 22:43:10 +0530

    Created Day 4

---
+++ Day_04/index.js
@@ -0,0 +1,37 @@
+// Task 1 :
+for (let i = 1; i <= 10; i++) {
+    console.log(i);
+}
+
+// Task 2 :
+let num = 5;
+for (let i = 1; i <= 10; i++) {
+    console.log(num * i);
+}
+
+// Task 3 :
+let sum = 0;
+let i = 1;
+while (i <= 10) {
+    sum += i;
+    i++;
+}
+console.log(sum);
+
+// Task 4 :
+let alterSum = 0;
+let j = 10;
+while (j) {
+    alterSum += j;
+    j--;
+}
+console.log(alterSum);
+
+// Task 5 :
+let val = 1;
+do {
+    console.log(val);
+    val++;
+} while(val <= 5)
+
+// Task 6 :
